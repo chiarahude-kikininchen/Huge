bf7fc08d0742b987234a9d3a6dd6398f406c7793
Chiara Hude <chiara.hude@gmail.com>
2026-05-12 14:32:31 +0200

    Disabled email verification

--- application/model/RegistrationModel.php
+++ application/model/RegistrationModel.php
@@ -18,12 +18,11 @@ class RegistrationModel
         // clean the input
         $user_name = strip_tags(Request::post('user_name'));
         $user_email = strip_tags(Request::post('user_email'));
-        $user_email_repeat = strip_tags(Request::post('user_email_repeat'));
         $user_password_new = Request::post('user_password_new');
         $user_password_repeat = Request::post('user_password_repeat');
 
         // stop registration flow if registrationInputValidation() returns false (= anything breaks the input check rules)
-        $validation_result = self::registrationInputValidation(Request::post('captcha'), $user_name, $user_password_new, $user_password_repeat, $user_email, $user_email_repeat);
+        $validation_result = self::registrationInputValidation(Request::post('captcha'), $user_name, $user_password_new, $user_password_repeat, $user_email);
         if (!$validation_result) {
             return false;
         }
@@ -50,13 +49,13 @@ class RegistrationModel
         // if Username or Email were false, return false
         if (!$return) return false;
 
-        // generate random hash for email verification (40 char string)
-        $user_activation_hash = sha1(uniqid(mt_rand(), true));
+        // no email verification needed because users are created by an admin
+        $user_activation_hash = null;
 
         // write user data to database
         if (!self::writeNewUserToDatabase($user_name, $user_password_hash, $user_email, time(), $user_activation_hash)) {
             Session::add('feedback_negative', Text::get('FEEDBACK_ACCOUNT_CREATION_FAILED'));
-            return false; // no reason not to return false here
+            return false;
         }
 
         // get user_id of the user that has been created, to keep things clean we DON'T use lastInsertId() here
@@ -67,16 +66,14 @@ class RegistrationModel
             return false;
         }
 
-        // send verification email
-        if (self::sendVerificationEmail($user_id, $user_email, $user_activation_hash)) {
-            Session::add('feedback_positive', Text::get('FEEDBACK_ACCOUNT_SUCCESSFULLY_CREATED'));
-            return true;
+        // activate user immediately because admins create users manually
+        if (!self::setNewUserActive($user_id)) {
+            Session::add('feedback_negative', Text::get('FEEDBACK_ACCOUNT_CREATION_FAILED'));
+            return false;
         }
 
-        // if verification email sending failed: instantly delete the user
-        self::rollbackRegistrationByUserId($user_id);
-        Session::add('feedback_negative', Text::get('FEEDBACK_VERIFICATION_MAIL_SENDING_FAILED'));
-        return false;
+        Session::add('feedback_positive', Text::get('FEEDBACK_ACCOUNT_SUCCESSFULLY_CREATED'));
+        return true;
     }
 
     /**
@@ -87,11 +84,10 @@ class RegistrationModel
      * @param $user_password_new
      * @param $user_password_repeat
      * @param $user_email
-     * @param $user_email_repeat
      *
      * @return bool
      */
-    public static function registrationInputValidation($captcha, $user_name, $user_password_new, $user_password_repeat, $user_email, $user_email_repeat)
+    public static function registrationInputValidation($captcha, $user_name, $user_password_new, $user_password_repeat, $user_email)
     {
         $return = true;
 
@@ -101,8 +97,8 @@ class RegistrationModel
             $return = false;
         }
 
-        // if username, email and password are all correctly validated, but make sure they all run on first sumbit
-        if (self::validateUserName($user_name) AND self::validateUserEmail($user_email, $user_email_repeat) AND self::validateUserPassword($user_password_new, $user_password_repeat) AND $return) {
+        // if username, email and password are all correctly validated, but make sure they all run on first submit
+        if (self::validateUserName($user_name) AND self::validateUserEmail($user_email) AND self::validateUserPassword($user_password_new, $user_password_repeat) AND $return) {
             return true;
         }
 
@@ -136,18 +132,12 @@ class RegistrationModel
      * Validates the email
      *
      * @param $user_email
-     * @param $user_email_repeat
      * @return bool
      */
-    public static function validateUserEmail($user_email, $user_email_repeat)
+    public static function validateUserEmail($user_email)
     {
         if (empty($user_email)) {
             Session::add('feedback_negative', Text::get('FEEDBACK_EMAIL_FIELD_EMPTY'));
-            return false;
-        }
-
-        if ($user_email !== $user_email_repeat) {
-            Session::add('feedback_negative', Text::get('FEEDBACK_EMAIL_REPEAT_WRONG'));
             return false;
         }
 
@@ -209,11 +199,11 @@ class RegistrationModel
                     VALUES (:user_name, :user_password_hash, :user_email, :user_creation_timestamp, :user_activation_hash, :user_provider_type)";
         $query = $database->prepare($sql);
         $query->execute(array(':user_name' => $user_name,
-                              ':user_password_hash' => $user_password_hash,
-                              ':user_email' => $user_email,
-                              ':user_creation_timestamp' => $user_creation_timestamp,
-                              ':user_activation_hash' => $user_activation_hash,
-                              ':user_provider_type' => 'DEFAULT'));
+            ':user_password_hash' => $user_password_hash,
+            ':user_email' => $user_email,
+            ':user_creation_timestamp' => $user_creation_timestamp,
+            ':user_activation_hash' => $user_activation_hash,
+            ':user_provider_type' => 'DEFAULT'));
         $count =  $query->rowCount();
         if ($count == 1) {
             return true;
@@ -223,8 +213,28 @@ class RegistrationModel
     }
 
     /**
-     * Deletes the user from users table. Currently used to rollback a registration when verification mail sending
-     * was not successful.
+     * Activates a newly created user immediately.
+     *
+     * @param int $user_id
+     * @return bool
+     */
+    public static function setNewUserActive($user_id)
+    {
+        $database = DatabaseFactory::getFactory()->getConnection();
+
+        $sql = "UPDATE users
+                SET user_active = 1, user_activation_hash = NULL
+                WHERE user_id = :user_id
+                LIMIT 1";
+
+        $query = $database->prepare($sql);
+        $query->execute(array(':user_id' => $user_id));
+
+        return $query->rowCount() == 1;
+    }
+
+    /**
+     * Deletes the user from users table.
      *
      * @param $user_id
      */
@@ -237,8 +247,8 @@ class RegistrationModel
     }
 
     /**
-     * Sends the verification email (to confirm the account).
-     * The construction of the mail $body looks weird at first, but it's really just a simple string.
+     * Sends the verification email.
+     * Kept for compatibility, but no longer used by admin-created registrations.
      *
      * @param int $user_id user's id
      * @param string $user_email user's email
@@ -249,7 +259,7 @@ class RegistrationModel
     public static function sendVerificationEmail($user_id, $user_email, $user_activation_hash)
     {
         $body = Config::get('EMAIL_VERIFICATION_CONTENT') . Config::get('URL') . Config::get('EMAIL_VERIFICATION_URL')
-                . '/' . urlencode($user_id) . '/' . urlencode($user_activation_hash);
+            . '/' . urlencode($user_id) . '/' . urlencode($user_activation_hash);
 
         $mail = new Mail;
         $mail_sent = $mail->sendMail($user_email, Config::get('EMAIL_VERIFICATION_FROM_EMAIL'),
